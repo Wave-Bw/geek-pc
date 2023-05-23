c5db597b0afdc6dfa10438a6d0299abaf1a4fabe
刘博文 <664371268@qq.com>
2023-05-23 21:21:50 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Login from './pages/Login'
+import Home from './pages/Layout'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				{/* 配置路由的规则 */}
+				<Routes>
+					<Route path="/home" component={Home}></Route>
+					<Route path="/login" component={Login}></Route>
+				</Routes>
+			</div>
+		</Router>
+	)
 }
 
-export default App;
+export default App
---
+++ src/pages/Login/index.js
@@ -0,0 +1,8 @@
+import { Component } from 'react'
+
+class Login extends Component {
+	render() {
+		return <div className="login">登录</div>
+	}
+}
+export default Login
